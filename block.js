3c17d7712d03d67086b21138a27457ca6f787948
Planet15 <parmar0815@gmail.com>
2019-07-10 23:33:13 -0400

    genesis block, and mine block are created. Also, test code in dev-test

--- block.js
+++ block.js
@@ -13,6 +13,18 @@ class Block {
             Hash     : ${this.hash.substring(0, 10)}
             Data     : ${this.data}`; 
     }
+    
+    static genesis() {
+        return new this('Genesis time', '-----', 'f1r57-h45h', []);
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = 'todo-hash';
+
+        return new this (timestamp, lastHash, hash, data);
+    }
 }
 
 module.exports = Block; 
