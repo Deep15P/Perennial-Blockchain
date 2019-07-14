3e109f2321953a5981ee03de62c770bf2284c0df
Planet15 <parmar0815@gmail.com>
2019-07-13 20:00:10 -0400

    created test to see it replaces chain with a valid chain or not

--- blockchain.test.js
+++ blockchain.test.js
@@ -37,5 +37,19 @@ describe('Blockchain', () => {
         bc2.chain[1].data = 'Not foo';
 
         expect(bc2.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).toEqual(bc2.chain);
+    })
+
+    it('does not replace the chain with one of less than or equal to length', () => {
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).not.toEqual(bc2.chain);
     })
 });
