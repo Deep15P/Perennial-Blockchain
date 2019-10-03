ecc86325a69e56da0b8b17148e13832deb0b5a6d
Git PERENNIAL <Parmar0815@Gmail.com>
2019-10-02 23:49:15 -0400

    wallet test

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,3 @@
-const Block = require('./block');
-
-// const block = new Block('gee', 'pat', 'bat', 'dog');
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
-
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-console.log(fooBlock.toString());
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
